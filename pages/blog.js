f5f3e17093f8126ed4ee967fddb9ed2bb6980a1b
Ankit Kumar Thakur <drankitkumarthakur@gmail.com>
2023-02-08 17:06:00 +0530

    ADD: blogpost in json created

--- pages/blog.js
+++ pages/blog.js
@@ -1,7 +1,36 @@
 import React from "react";
+import styles from "@/styles/Blog.module.css";
+import Link from "next/link";
 
+//Step 1: Collect all the files from blogdata directory
+//Step 2: Iterate through them and display them
 const Blog = () => {
-  return <div>This the blog home page</div>;
+  return (
+    <div className={styles.blogs}>
+      <main className={styles.main}>
+        <div>
+          <Link href={"/blogpost/learn-javascript"}>
+            <h3 className={styles.blogItem3}>
+              How to learn JavaScript in 2023 ?
+            </h3>
+          </Link>
+          <p>JavaScript is a language used fo design logic for the web.</p>
+        </div>
+        <div className={styles.blogItem}>
+          <h3>How to learn JavaScript in 2023 ?</h3>
+          <p>JavaScript is a language used fo design logic for the web.</p>
+        </div>
+        <div className={styles.blogItem}>
+          <h3>How to learn JavaScript in 2023 ?</h3>
+          <p>JavaScript is a language used fo design logic for the web.</p>
+        </div>
+        <div className={styles.blogItem}>
+          <h3>How to learn JavaScript in 2023 ?</h3>
+          <p>JavaScript is a language used fo design logic for the web.</p>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Blog;
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,10 +1,25 @@
 import React from "react";
 import { useRouter } from "next/router";
+import styles from "@/styles/BlogPost.module.css";
 
+//Step 3: Find the file corresponding to the slug
+//Step 4: Populate them inside the page
 const Slug = () => {
   const router = useRouter();
   const { slug } = router.query;
-  return <div>{slug}</div>;
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1>Title of the page {slug}</h1>
+        <hr />
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In officia
+          quis asperiores labore cupiditate cum commodi cumque. Beatae suscipit
+          molestias tenetur molestiae ipsam.
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Slug;
