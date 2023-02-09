eb97fe147d5203eef15fb09152fcb80b425d5cd3
Ankit Kumar Thakur <drankitkumarthakur@gmail.com>
2023-02-09 10:15:36 +0530

    ADD: getblog endpints are linked to frontend

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,10 +1,29 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import * as fs from "fs";
 
-export default function handler(req, res) {
-  fs.readdir("blogdata", (err, data) => {
-    console.log(data);
-    res.status(200).json(data);
-  });
-  // console.log(req);
+export default async function handler(req, res) {
+  let data = await fs.promises.readdir("blogdata");
+  let myFile;
+  let allBlogs = [];
+  // res.status(200).json(data);
+
+  for (let index = 0; index < data.length; index++) {
+    const item = data[index];
+    console.log(item);
+    myFile = await fs.promises.readFile("blogdata/" + item, "utf-8");
+    allBlogs.push(JSON.parse(myFile));
+  }
+  res.status(200).json(allBlogs);
+  // fs.readdir("blogdata", (err, data) => {
+  //   console.log(data);
+  //   let allblogs = [];
+  //   data.forEach((item) => {
+  //     console.log(item);
+  //     fs.readFile("blogdata/" + item, (d) => {
+  //       allblogs.push(d);
+  //     });
+  //   });
+  //   res.status(200).json(allblogs);
+  // });
+  // // console.log(req);
 }
--- pages/blog.js
+++ pages/blog.js
@@ -1,22 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "@/styles/Blog.module.css";
 import Link from "next/link";
 
 //Step 1: Collect all the files from blogdata directory
 //Step 2: Iterate through them and display them
 const Blog = () => {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    // console.log("useEffect is running");
+    fetch("http://localhost:3000/api/blogs")
+      .then((a) => {
+        return a.json();
+      })
+      .then((parsed) => {
+        setBlogs(parsed);
+      });
+  }, []);
   return (
     <div className={styles.blogs}>
       <main className={styles.main}>
-        <div>
-          <Link href={"/blogpost/learn-javascript"}>
-            <h3 className={styles.blogItem3}>
-              How to learn JavaScript in 2023 ?
-            </h3>
-          </Link>
-          <p>JavaScript is a language used fo design logic for the web.</p>
-        </div>
-        <div className={styles.blogItem}>
+        {blogs.map((blogItem) => {
+          return (
+            <div key={blogItem.slug}>
+              <Link href={`/blogpost/${blogItem.slug}`}>
+                <h3 className={styles.blogItem3}>{blogItem.title}</h3>
+              </Link>
+              <p className={styles.blogItemp}>
+                {blogItem.content.substring(0, 140)}...
+              </p>
+            </div>
+          );
+        })}
+
+        {/* <div className={styles.blogItem}>
           <h3>How to learn JavaScript in 2023 ?</h3>
           <p>JavaScript is a language used fo design logic for the web.</p>
         </div>
@@ -27,7 +43,7 @@ const Blog = () => {
         <div className={styles.blogItem}>
           <h3>How to learn JavaScript in 2023 ?</h3>
           <p>JavaScript is a language used fo design logic for the web.</p>
-        </div>
+        </div> */}
       </main>
     </div>
   );
